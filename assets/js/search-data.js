aacab8064e4b944d1d93c7cbaaa32fa448ebb330
aks-psu <106095622+aks-psu@users.noreply.github.com>
2024-12-28 23:57:42 +0000

    Deploying to gh-pages from @ aks-psu/aks-psu.github.io@3d0ed20aeb271f95ef80f3c35269f1d8739415b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,15 +431,15 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-started-my-phd-emoji-sparkles-smile",
+          title: 'Started my PhD emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-started-my-phd-emoji-sparkles-smile",
           title: 'Started my PhD emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
